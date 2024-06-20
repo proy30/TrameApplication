4f2b7716e75379802fc6881651e2ef145b4c32e6
Parthib Roy <proy@lbl.gov>
2024-06-19 20:18:10 -0700

    Added state change notiications when inputParameters are adjusted, as well as conversion for kin_energy Units
    also some minor code adjusts

--- UI/content.py
+++ UI/content.py
@@ -161,13 +161,44 @@ class distributionParameters:
 
 class inputParameters:
     def __init__ (self):
-        pass
+        state.particle_shape = 1
+        state.npart = 10000
+        state.kin_energy_MeV = 2.0e3
+        state.bunch_charge_C = 1.0e-9
+        state.kin_energy_unit = "MeV"
 
-    def reset_parameters():
-            state.particle_shape = 1
-            state.npart = 10000
-            state.kin_energy_MeV = 2.0e3
-            state.bunch_charge_C = 1.0e-9
+
+    @state.change("particle_shape")
+    def a(particle_shape, **kwargs):
+        print(f"Particle Shape changed to: {particle_shape}")
+
+    @state.change("npart")
+    def on_npart_change(npart, **kwargs):
+        print(f"# of Particles changed to: {npart}")
+
+    @state.change("kin_energy_MeV")
+    def on_kin_energy_change(kin_energy_MeV, **kwargs):
+        print(f"Kinetic Energy changed to: {kin_energy_MeV}")
+
+    @state.change("bunch_charge_C")
+    def on_bunch_charge_C_change(bunch_charge_C, **kwargs):
+        print(f"Bunch Charge (C) changed to: {bunch_charge_C}")
+
+    @state.change("kin_energy_unit")
+    def b(kin_energy_unit, **kwargs):
+        inputParameters.convert_kin_energy()
+        print(f"Kinetic Energy changed to: {kin_energy_unit}")
+
+    def convert_kin_energy():
+        conversion_factors = {
+            "meV": 1.0e9,
+            "eV": 1.0e6,
+            "MeV": 1.0,
+            "GeV": 1.0e-3,
+            "TeV": 1.0e-6,
+        }
+        state.kin_energy_MeV = float(state.kin_energy_MeV)
+        state.kin_energy_MeV *= conversion_factors[state.kin_energy_unit]
 
     def card(self):
         with vuetify.VCard(classes="ma-2", style="max-width: 340px; height: 320px"):
@@ -176,15 +207,15 @@ class inputParameters:
                     "mdi-refresh",
                     classes="ml-2",
                     color="primary",
-                    click=inputParameters.reset_parameters
+                    click=resetParameters.reset_inputParameters
                 )
             vuetify.VDivider()
 
             with vuetify.VCardText():
                 vuetify.VSelect(
                     label="Particle Shape",
-                    v_model="particle_shape",
-                    items=["1", "2", "3"],
+                    v_model=("particle_shape",),
+                    items=([1, 2, 3],),
                     dense=True,
                     classes="mb-2"
                 )
@@ -202,12 +233,13 @@ class inputParameters:
                             v_model=("kin_energy_MeV",),
                             type="number",
                             dense=True,
-                            classes="mr-2"
+                            classes="mr-2",
                         )
                     with vuetify.VCol(cols=4):
                         vuetify.VSelect(
                             label="Unit",
-                            items=(["eV", "meV", "MeV", "GeV", "TeV"],),
+                            v_model=("kin_energy_unit",),
+                            items=(["meV", "eV", "MeV", "GeV", "TeV"],),
                             dense=True,
                         )
                 vuetify.VTextField(
@@ -275,14 +307,11 @@ class resetParameters:
             "muypy": 0.846574929020762,
             "mutpt": 0.0
         }
-    def reset_parameters():
+
+    def reset_inputParameters():
         state.particle_shape = 1
         state.npart = 10000
         state.kin_energy_MeV = 2.0e3
         state.bunch_charge_C = 1.0e-9
-        state.image_data = None
-        state.slice_step_diagnostics = False   
-        state.space_charge = False
-        state.selected_lattice = None
-        state.selected_distribution = "waterbag"
+        state.kin_energy_unit = "MeV"
 
