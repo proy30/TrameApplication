c3981a1fdc263a98889921e79b254bc1b062171b
Parthib Roy <proy@lbl.gov>
2024-06-29 14:22:38 -0700

    refactor/Started refactoring latticeConfiguration

---
+++ Input/latticeConfigurationCard/lattice/main.py
@@ -0,0 +1,77 @@
+
+from trame.app import get_server
+from trame.ui.vuetify import SinglePageWithDrawerLayout
+from trame.widgets import vuetify
+
+from functions  import selectClasses
+from impactx import elements
+# -----------------------------------------------------------------------------
+# Trame setup
+# -----------------------------------------------------------------------------
+
+server = get_server(client_type="vue2")
+state, ctrl = server.state, server.controller
+
+# -----------------------------------------------------------------------------
+# Helpful
+# -----------------------------------------------------------------------------
+state.listOfLatticeElements = selectClasses(elements)
+
+
+# -----------------------------------------------------------------------------
+# Callbacks
+# -----------------------------------------------------------------------------
+@state.change("listOfLatticeElements")
+def on_lattice_element_changed(listOfLatticeElements, **kwargs):
+    print (f"Lattice Selection Changed to: {listOfLatticeElements}")
+# -----------------------------------------------------------------------------
+# ContentSetup
+# -----------------------------------------------------------------------------
+class latticeConfiguration:
+    def card(self):
+        with vuetify.VCard(classes="ma-2", style="width: 712px;"):
+            with vuetify.VCardTitle("Lattice Configuration"):
+                vuetify.VIcon(
+                    "mdi-information",
+                    classes="ml-2",
+                    style="color: #00313C;",
+                )
+            vuetify.VDivider()
+            with vuetify.VCardText():
+                with vuetify.VRow(classes="mb-1 align-center", no_gutters=True):
+                    with vuetify.VCol(cols=8):
+                        vuetify.VSelect(
+                            label="Select Accelerator Lattice",
+                            items=("listOfLatticeElements",),
+                            dense=True,
+                            classes="mr-2 mt-6"
+                        )
+                    with vuetify.VCol(cols="auto"):
+                        vuetify.VBtn(
+                            "ADD",
+                            color="primary",
+                            dense=True,
+                            classes="mr-2"
+                        )
+                    with vuetify.VCol(cols="auto"):
+                        vuetify.VBtn(
+                            "CLEAR",
+                            color="secondary",
+                            dense=True
+                        )
+
+
+latticeConfiguration =  latticeConfiguration()
+with SinglePageWithDrawerLayout(server) as layout:
+    with layout.content:
+        with vuetify.VContainer(fluid=True):
+            with vuetify.VRow(no_gutters=True):
+                with vuetify.VCol(cols="auto", classes="pa-2"):
+                    latticeConfiguration.card()
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    server.start()
