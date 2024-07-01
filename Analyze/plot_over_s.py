e2d18df9288a1ce0065a821baa415b93ed08731c
Parthib Roy <proy@lbl.gov>
2024-07-01 15:49:18 -0700

    Added functionality - Data table only displays selected columns, rather than selected data
    Previously, the data table displayed every single header, and displayed only the headers selected. Now the displayed headers are only the ones that are selected

--- Analyze/plot_over_s.py
+++ Analyze/plot_over_s.py
@@ -4,6 +4,7 @@ from trame.widgets import vuetify
 
 from widgets import Functions
 
+
 # -----------------------------------------------------------------------------
 # Start server
 # -----------------------------------------------------------------------------
@@ -14,25 +15,25 @@ state, ctrl = server.state, server.controller
 # -----------------------------------------------------------------------------
 # Defaults
 # -----------------------------------------------------------------------------
+
 reducedBeam_data = '/mnt/c/Users/parth/Downloads/vsCode/fixBugs/diags/reduced_beam_characteristics.0.0'
 refParticle_data = '/mnt/c/Users/parth/Downloads/vsCode/fixBugs/diags/ref_particle.0.0'
+default_headers = ["step", "s", "z"]
 
-combined_data = Functions.combine_files(reducedBeam_data, refParticle_data)
-dictionary_data = Functions.convert_to_dict(combined_data)
+combined_files= Functions.combine_files(reducedBeam_data, refParticle_data)
+combined_files_data_converted_to_dictionary_format = Functions.convert_to_dict(combined_files)
 
-data, headers = dictionary_data
-default_columns = ["step", "s"]
-all_data_columns = [header['value'] for header in headers]
 
-state.data = data
-state.headers = headers
-state.selected_columns = default_columns
-state.all_columns = all_data_columns
+data, headers = combined_files_data_converted_to_dictionary_format
+state.all_data = data
+state.all_headers = headers
+state.selected_headers = default_headers
 
-@state.change("selected_columns")
-def update_table(selected_columns, **kwargs):
-    # state.data = Functions.filter_data_by_columns(data, selected_columns)
-    state.selected_columns = [header for header in headers if header['value'] in selected_columns]
+@state.change("selected_headers")
+def on_header_selection_change(selected_headers, **kwargs):
+    state.filtered_headers = Functions.filter_headers(state.all_headers, selected_headers)
+    state.filtered_data = Functions.filter_data(state.all_data, selected_headers)
+
 # -----------------------------------------------------------------------------
 # GUI
 # -----------------------------------------------------------------------------
@@ -43,16 +44,17 @@ class Table:
                 vuetify.VCardTitle("Table")
                 with vuetify.VCol(style="max-width: 500px;"):
                     vuetify.VSelect(
-                        v_model=("selected_columns",),
-                        items=("all_columns",),
+                        v_model=("selected_headers",),
+                        items=("all_headers",),
                         label="Select data to view",
                         multiple=True,
                     )
             vuetify.VDivider()
             vuetify.VDataTable(
-                headers=("headers",),
-                items=("data",),
-                header_class="centered-header"
+                headers=("filtered_headers",),
+                items=("filtered_data",),
+                header_class="centered-header",
+                dense=True
             )
 
 # -----------------------------------------------------------------------------
