c5c6ae72077cfd304e3817806e03ee9b38ae22ed
Parthib Roy <proy@lbl.gov>
2024-07-02 10:52:38 -0700

    Added sample 1D plot
    located to right side of data table, with fixed values

--- Analyze/plot_over_s.py
+++ Analyze/plot_over_s.py
@@ -1,8 +1,9 @@
 from trame.app import get_server
 from trame.ui.vuetify import SinglePageWithDrawerLayout
-from trame.widgets import vuetify
+from trame.widgets import vuetify, plotly
 
 from widgets import Functions
+import plotly.graph_objects as go
 
 
 # -----------------------------------------------------------------------------
@@ -11,6 +12,35 @@ from widgets import Functions
 
 server = get_server(client_type="vue2")
 state, ctrl = server.state, server.controller
+
+# -----------------------------------------------------------------------------
+# 1D Plot
+# -----------------------------------------------------------------------------
+
+def line_plot():
+    x_axis = [1,2,3,4]
+    y_axis =[2,3,4,5,7,12]
+
+    return go.Figure(
+        data=go.Scatter(
+            x = x_axis,
+            y = y_axis,
+            mode='lines+markers',
+            line = dict(color='blue', width=2),
+            marker=dict(size=8)
+        ),
+        layout=go.Layout(
+            title="Interactive 1D plot over s",
+            xaxis=dict(title="s"),
+            yaxis=dict(title="Y axis"),
+            margin=dict(l=20, r=20, t=25, b=30)
+        )
+    )
+    
+
+PLOTS = {
+    "Line": line_plot
+}
 
 # -----------------------------------------------------------------------------
 # Defaults
@@ -23,16 +53,28 @@ default_headers = ["step", "s", "z"]
 combined_files= Functions.combine_files(reducedBeam_data, refParticle_data)
 combined_files_data_converted_to_dictionary_format = Functions.convert_to_dict(combined_files)
 
-
 data, headers = combined_files_data_converted_to_dictionary_format
 state.all_data = data
 state.all_headers = headers
 state.selected_headers = default_headers
 
+# -----------------------------------------------------------------------------
+# State changes
+# -----------------------------------------------------------------------------
+
 @state.change("selected_headers")
 def on_header_selection_change(selected_headers, **kwargs):
     state.filtered_headers = Functions.filter_headers(state.all_headers, selected_headers)
     state.filtered_data = Functions.filter_data(state.all_data, selected_headers)
+
+@state.change("filtered_data")
+def on_filtered_data_change(filtered_data, **kwargs):
+    ctrl.figure_update(line_plot())
+
+@state.change("active_plot")
+def on_plot_selection_change(active_plot, **kwargs):
+    ctrl.figure.update(PLOTS[active_plot]())
+
 
 # -----------------------------------------------------------------------------
 # GUI
@@ -54,8 +96,13 @@ class Table:
                 headers=("filtered_headers",),
                 items=("filtered_data",),
                 header_class="centered-header",
-                dense=True
+                dense=True,
+                height="250px",
             )
+    def plot():
+        figure = plotly.Figure(display_mode_bar="true")
+        ctrl.figure_update = figure.update
+        ctrl.figure_update(line_plot())
 
 # -----------------------------------------------------------------------------
 # Main Layout
@@ -63,9 +110,11 @@ class Table:
 with SinglePageWithDrawerLayout(server) as layout:
     with layout.content:
         with vuetify.VContainer(fluid=True):
-            with vuetify.VRow(no_gutters=True):
-                with vuetify.VCol(cols="auto", classes="pa-2"):
+            with vuetify.VRow(no_gutters=True, classes="fill-height"):
+                with vuetify.VCol(cols="auto", classes="pa-2 fill-height"):
                     Table.card()
+                with vuetify.VCol(classes="pa-2 d-flex align-center justify-center fill-height"):
+                    Table.plot()
 
 # -----------------------------------------------------------------------------
 # Main
