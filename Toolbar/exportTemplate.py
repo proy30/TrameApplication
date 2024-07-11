b6a58232a8bc7b603ee1c3d5f0fd7eeb6c379aa6
Parthib Roy <proy@lbl.gov>
2024-07-11 11:44:09 -0700

    Export template now includes algorithm/diagnostics

--- Toolbar/exportTemplate.py
+++ Toolbar/exportTemplate.py
@@ -1,5 +1,6 @@
 
 from trame.app import get_server
+import re
 
 # -----------------------------------------------------------------------------
 # Trame setup
@@ -12,7 +13,25 @@ state, ctrl = server.state, server.controller
 # Content
 # -----------------------------------------------------------------------------
 
+import re
+
 def retrieve_state_content():
+    # Read the file contents
+    with open('output_distribution_parameters.txt', 'r') as file:
+        file_content = file.read()
+
+    # Regular expression to extract lambda and mu values
+    lambdaX = re.search(r'lambdaX=(\d+)', file_content).group(1)
+    lambdaY = re.search(r'lambdaY=(\d+)', file_content).group(1)
+    lambdaT = re.search(r'lambdaT=(\d+)', file_content).group(1)
+    lambdaPx = re.search(r'lambdaPx=(\d+)', file_content).group(1)
+    lambdaPy = re.search(r'lambdaPy=(\d+)', file_content).group(1)
+    lambdaPt = re.search(r'lambdaPt=(\d+)', file_content).group(1)
+    muxpx = re.search(r'muxpx=(\d+\.\d+)', file_content).group(1)
+    muypy = re.search(r'muypy=(\d+\.\d+)', file_content).group(1)
+    mutpt = re.search(r'mutpt=(\d+\.\d+)', file_content).group(1)
+
+    # Build the content string with extracted values
     content = f"""###############################################################################
 # Particle Beam(s)
 ###############################################################################
@@ -22,14 +41,33 @@ beam.kin_energy = {state.kin_energy_MeV}
 beam.charge = {state.charge}
 beam.particle = electron
 beam.distribution = {state.selectedDistribution}
-beam.lambdaX = {state.lambdaX} 
-beam.lambdaY = {state.lambdaY} 
-beam.lambdaT = {state.lambdaT} 
-beam.lambdaPx = {state.lambdaPx}
-beam.lambdaPy = {state.lambdaPy}
-beam.lambdaPt = {state.lambdaPt}
-beam.muxpx = 0.0
-beam.muypy = 0.0
-beam.mutpt = 0.0
+beam.lambdaX = {lambdaX} 
+beam.lambdaY = {lambdaY} 
+beam.lambdaT = {lambdaT} 
+beam.lambdaPx = {lambdaPx}
+beam.lambdaPy = {lambdaPy}
+beam.lambdaPt = {lambdaPt}
+beam.muxpx = {muxpx}
+beam.muypy = {muypy}
+beam.mutpt = {mutpt}
+
+
+###############################################################################
+# Beamline: lattice elements and segments
+###############################################################################
+
+###############################################################################
+# Algorithms
+###############################################################################
+algo.particle_shape = {state.particle_shape}
+algo.space_charge = {state.space_charge}
+
+
+###############################################################################
+# Diagnostics
+###############################################################################
+diag.slice_step_diagnostics = {state.slice_step_diagnostics}
+
 """ 
     return content
+
