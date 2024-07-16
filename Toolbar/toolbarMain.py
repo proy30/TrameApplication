d4fcb7f98bdfc9c5c1b9a6a57263ebfc5d2306db
Parthib Roy <proy@lbl.gov>
2024-07-16 15:58:32 -0700

    Made toolbar elements more static
    Instead of applying the same components multiple times, a function to that component is called. Helps with code readability and maintainy consistent toolbar components

--- Toolbar/toolbarMain.py
+++ Toolbar/toolbarMain.py
@@ -11,7 +11,7 @@ state, ctrl = server.state, server.controller
 
 state.selectedWorkflow = "Optimize Triplet"
 state.isSelectedWorkflow = ""
-state.selectedVisualization = "Twiss Phase Space Ellipses"
+state.selectedVisualization = ""
 # -----------------------------------------------------------------------------
 # Trigger
 # -----------------------------------------------------------------------------
@@ -25,35 +25,71 @@ def on_selectedWorkflow_change(selectedWorkflow, **kwargs):
     print(f"Selected workflow is {selectedWorkflow}")
     if selectedWorkflow == None:
         state.isSelectedWorkflow = "Please select a workflow"
+
 # -----------------------------------------------------------------------------
-# Content
+# Common toolbar elements
 # -----------------------------------------------------------------------------
 
-class toolbars:
+class toolbarElements:
     
-    def analyzeToolbar():
-        vuetify.VSpacer()
+    def select_workflow():
+        vuetify.VCombobox(
+        placeholder="Select Workflow",
+        v_model=("selectedWorkflow",),
+        items=(["DataFrameTest", "Optimize Triplet"],),
+        clearable=True,
+        error_messages=("isSelectedWorkflow",),
+        dense=True,
+        hide_details=True,
+        style="max-width: 175px",
+        classes="mr-2",
+    )
+    
+    def select_visualization():
+        vuetify.VCombobox(
+            placeholder="Select Visualization",
+            v_model=("selectedVisualization",),
+            items=(["Twiss Phase Space Ellipses", "Lattice Visualization"],),
+            clearable=True,
+            dense=True,
+            hide_details=True,
+            style="max-width: 250px",
+        )
+
+    def plot_options():
         vuetify.VSelect(
             v_model=("active_plot", "1D plots over s"),
             items=("plot_options",),
             label="Select plot to view",
             hide_details=True,
             dense=True,
-            style="width: 100px;",
+            style="max-width: 250px",
             disabled=("disableRunSimulationButton",True),
         )
+
+    def run_simulation_button():
         vuetify.VBtn(
             "Run Simulation",
             style="background-color: #00313C; color: white; margin: 0 20px;",
             click=ctrl.run_simulation,
             disabled=("disableRunSimulationButton",True),
         )
+    
+    def export_input_data():
+        vuetify.VIcon(
+            "mdi-download",
+            style="color: #00313C; margin: 0 10px;",
+            click="utils.download('input.in', trigger('export'), 'text/plain')",
+            disabled=("disableRunSimulationButton",True),
+        )
+
+    def switch_theme():
         vuetify.VSwitch(
-                v_model="$vuetify.theme.dark",
-                hide_details=True,
-            )
-        
-    def latticeToolbar():
+            v_model="$vuetify.theme.dark",
+            hide_details=True,
+        )
+
+    def file_upload():
         vuetify.VFileInput(
             #Allows users to upload file, but nothing more than that.
             label="Upload Input File",
@@ -64,40 +100,26 @@ class toolbars:
             hide_details=True,
             style="max-width: 175px;",
         )
+    
+# -----------------------------------------------------------------------------
+# Content
+# -----------------------------------------------------------------------------
+
+class toolbars:
+    
+    def analyzeToolbar():
         vuetify.VSpacer()
-        vuetify.VCombobox(
-            placeholder="Select Workflow",
-            v_model=("selectedWorkflow",),
-            items=(["DataFrameTest", "Optimize Triplet"],),
-            clearable=True,
-            error_messages=("isSelectedWorkflow",),
-            dense=True,
-            hide_details=True,
-            style="max-width: 175px",
-            classes="mr-2",
-        )
-        vuetify.VCombobox(
-            placeholder="Select Visualization",
-            v_model=("selectedVisualization",),
-            items=(["Twiss Phase Space Ellipses", "Lattice Visualization"],),
-            clearable=True,
-            dense=True,
-            hide_details=True,
-            style="max-width: 250px",
-        )
-        vuetify.VBtn(
-            "Run Simulation",
-            style="background-color: #00313C; color: white; margin: 0 10px;",
-            click=ctrl.run_simulation,
-            disabled=("disableRunSimulationButton",True),
-        )
-        vuetify.VIcon(
-            "mdi-download",
-            style="color: #00313C; margin: 0 10px;",
-            click="utils.download('input.in', trigger('export'), 'text/plain')",
-            disabled=("disableRunSimulationButton",True),
-        )
-        vuetify.VSwitch(
-                v_model="$vuetify.theme.dark",
-                hide_details=True,
-            )
+        toolbarElements.select_workflow()
+        toolbarElements.plot_options()
+        toolbarElements.run_simulation_button()
+        toolbarElements.export_input_data()
+        toolbarElements.switch_theme()
+        
+    def latticeToolbar():
+        toolbarElements.file_upload()
+        vuetify.VSpacer()
+        toolbarElements.select_workflow()
+        toolbarElements.select_visualization()
+        toolbarElements.run_simulation_button()
+        toolbarElements.export_input_data()
+        toolbarElements.switch_theme()
